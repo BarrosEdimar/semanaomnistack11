dee9c5fdd4637a08728d3fdacf5ba4569216593b
Edimar <edimarbarros90@gmail.com>
2020-03-25 11:26:25 -0300

    OngController

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 const connection = require('../database/connection');
 
 module.exports = {
-  async index(require, response) {
+  async index(request, response) {
     const ongs = await connection('ongs').select('*');
   
     return response.json(ongs);
